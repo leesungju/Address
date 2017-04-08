151efb7eb3d33f133023b13d64a61588c656f7c3
leesungju <leesungju@t5online.com>
2017-04-08 16:00:03 +0900

    - 앨범/사진찍기 구현

--- Address/class/view/contacts/ContactsDetailViewController.m
+++ Address/class/view/contacts/ContactsDetailViewController.m
@@ -8,7 +8,9 @@
 
 #import "ContactsDetailViewController.h"
 
-@interface ContactsDetailViewController ()
+@interface ContactsDetailViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIButton *imageSelectBtn;
 
 @end
 
@@ -27,5 +29,43 @@
 - (IBAction)backAction:(id)sender {
     [[GUIManager sharedInstance] backControllerWithAnimation:YES];
 }
+
+- (IBAction)imageSelectAction:(id)sender {
+    
+    UIImagePickerController * picker = [UIImagePickerController new];
+    picker.allowsEditing = YES;
+    [picker setDelegate:self];
+    
+    UIAlertController *av = [UIAlertController alertControllerWithTitle:@"알림" message:@"선택하세요!" preferredStyle:UIAlertControllerStyleActionSheet];
+    [av addAction:[UIAlertAction actionWithTitle:@"카메라" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
+                   {
+                       [picker setSourceType:UIImagePickerControllerSourceTypeCamera];
+                       [self presentViewController:picker animated:YES completion:nil];
+                   }]];
+    [av addAction:[UIAlertAction actionWithTitle:@"앨범" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
+                   {
+                       [picker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+                       [self presentViewController:picker animated:YES completion:nil];
+                   }]];
+    [av addAction:[UIAlertAction actionWithTitle:@"취소" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action)
+                   {
+
+                   }]];
+    [self presentViewController:av animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    NSLog(@"%@",info);
+    UIImage * img = [info valueForKey:UIImagePickerControllerEditedImage];
+    [_imageView setImage:img];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    
+}
+
+
 
 @end
